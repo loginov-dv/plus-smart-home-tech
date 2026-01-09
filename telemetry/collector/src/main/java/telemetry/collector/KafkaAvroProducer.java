1a35894fbbcd6cab9ba53b886cc8bda175f9ee90
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-01-09 16:11:30 +0300

    feat(telemetry.collector): добавить бин KafkaAvroProducer

---
+++ telemetry/collector/src/main/java/telemetry/collector/KafkaAvroProducer.java
@@ -0,0 +1,49 @@
+package telemetry.collector;
+
+import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.VoidSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+@Component
+@Slf4j
+public class KafkaAvroProducer {
+
+    private final KafkaProducer<Void, SpecificRecordBase> producer;
+
+    public KafkaAvroProducer(@Value("${telemetry.collector.kafka.bootstrap.servers}") String serverUrl) {
+        log.info("Using kafka server at url: {}", serverUrl);
+
+        Properties config = new Properties();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, serverUrl);
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
+
+        producer = new KafkaProducer<>(config);
+    }
+
+    public Future<RecordMetadata> send(String topic, SpecificRecordBase message) {
+        ProducerRecord<Void, SpecificRecordBase> record = new ProducerRecord<>(topic, message);
+        log.debug("Sending message to topic [{}]: {}", topic, message);
+
+        return producer.send(record);
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        if (producer != null) {
+            producer.flush();
+            producer.close();
+        }
+    }
+}
