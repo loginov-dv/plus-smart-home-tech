f4bde6ca31d02664ce1ab579748f9c54753b98c0
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-01-29 21:06:22 +0300

    refactor(telemetry.collector): deprecate ErrorHandler

--- telemetry/collector/src/main/java/telemetry/collector/controller/ErrorHandler.java
+++ telemetry/collector/src/main/java/telemetry/collector/controller/ErrorHandler.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import telemetry.collector.exception.HandlerNotFoundException;
 import telemetry.collector.exception.UnknownTypeException;
@@ -19,8 +18,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Deprecated
 @Slf4j
-@RestControllerAdvice
 public class ErrorHandler {
     private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
