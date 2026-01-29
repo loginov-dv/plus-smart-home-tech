b0abdfeffd65d26e41e430a3a6864d9223aac979
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-01-29 21:09:11 +0300

    feat(telemetry.analyzer): добавить исключения

---
+++ telemetry/analyzer/src/main/java/telemetry/analyzer/exception/UnknownConditionValueException.java
@@ -0,0 +1,7 @@
+package telemetry.analyzer.exception;
+
+public class UnknownConditionValueException extends RuntimeException {
+    public UnknownConditionValueException(String message) {
+        super(message);
+    }
+}
