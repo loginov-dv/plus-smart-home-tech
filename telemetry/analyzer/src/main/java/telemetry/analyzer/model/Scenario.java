98fdd039d36112e6d77027ce8b85dc1c4d9c5966
DmitryLoginov <v94-gb4h4@yandex.ru>
2026-01-29 21:07:00 +0300

    feat(telemetry.analyzer): добавить энтити-классы

---
+++ telemetry/analyzer/src/main/java/telemetry/analyzer/model/Scenario.java
@@ -0,0 +1,37 @@
+package telemetry.analyzer.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(
+        name = "scenarios",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"hud_id", "name"})
+)
+@Getter
+@Setter
+@ToString
+public class Scenario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "hub_id")
+    private String hubId;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "scenario", cascade = CascadeType.REMOVE)
+    @ToString.Exclude
+    private Set<ScenarioCondition> scenarioConditions = new HashSet<>();
+
+    @OneToMany(mappedBy = "scenario", cascade = CascadeType.REMOVE)
+    @ToString.Exclude
+    private Set<ScenarioAction> scenarioActions = new HashSet<>();
+}
